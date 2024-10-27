1d2f13b2044bf9292559abdef834570b4be6bc9f
tojry <theojoffroy@gmail.com>
2024-10-27 15:01:34 +0100

    Initialisation backend

---
+++ backend/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import * as Config from 'config';
+import { AppConfig } from './app.types';
+
+async function bootstrap(config: AppConfig) {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  await app.listen(config.port, config.host);
+  Logger.log(
+    `Application served at http://${config.host}:${config.port}`,
+    'bootstrap',
+  );
+}
+
+bootstrap(Config.get<AppConfig>('server'));
